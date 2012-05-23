1f0d045c1d79b3b7991edcd11b5a3f33cae22737
Shawn Smith <shawnpsmith@gmail.com>
2012-05-22 22:48:44 -0700

    add info.rkt for planet package information

---
+++ info.rkt
@@ -0,0 +1,6 @@
+#lang setup/infotab
+
+(define name "myGengo API client")
+(define blurb '("A Racket client for the myGengo API."))
+(define primary-file "mygengo.rkt")
+(define categories '(misc devtools))
